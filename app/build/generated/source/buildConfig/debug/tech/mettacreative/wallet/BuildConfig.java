cc8d42fe0be27a568c3faa0f1758c1f08bd12f6d
srihari kapu <sriharikapu163@gmail.com>
2019-02-07 14:16:26 +0800

    Initial Commit

---
+++ app/build/generated/source/buildConfig/debug/tech/mettacreative/wallet/BuildConfig.java
@@ -0,0 +1,17 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package tech.mettacreative.wallet;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "tech.mettacreative.wallet";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 10;
+  public static final String VERSION_NAME = "2.2.0";
+  // Fields from default config.
+  public static final String ETHERSCAN_API_KEY = "Y5FHVYEZRBH3VHB263GQ3IIIBM1CQRAYWK";
+  public static final String INFURA_API_KEY = "9568443a5cbc4585891777aabc099cd8";
+  public static final String SEGMENT_API_KEY = "secret";
+}
